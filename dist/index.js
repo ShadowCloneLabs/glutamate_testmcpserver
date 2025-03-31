030828a3690d1f73ea6725786b787917d6928a49
Pramod Geddam <shadowclonelabs@gmail.com>
2025-04-01 01:45:53 +0530

    build fixes

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,13 @@
-import { SequentialThinkingHttpServer } from "./server2.js";
+import { SequentialThinkingHttpServer } from './server2.js';
 async function main() {
-  try {
-    const server = new SequentialThinkingHttpServer();
-    // Start the HTTP server on port 3000
-    await server.startHttpServer(4002);
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
+    try {
+        const server = new SequentialThinkingHttpServer();
+        // Start the HTTP server on port 3000
+        await server.startHttpServer(4002);
+    }
+    catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
 }
 main();
