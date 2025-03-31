d6f1a904e7fd23d86cb763b326cbf1132361d994
Pramod Geddam <shadowclonelabs@gmail.com>
2025-04-01 01:22:16 +0530

    update the port

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ async function main() {
   try {
     const server = new SequentialThinkingHttpServer();
     // Start the HTTP server on port 3000
-    await server.startHttpServer(3002);
+    await server.startHttpServer(4002);
   } catch (error) {
     console.error("Failed to start server:", error);
     process.exit(1);
